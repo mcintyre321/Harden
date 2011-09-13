a062bbe2d4528db4e57b72b4795a7f47f35e3752
mcintyre321 <mcintyre321@gmail.com>
2011-09-13 19:02:54 +0100

    Added Create and .Harden extension

--- Harden/Hardener.cs
+++ Harden/Hardener.cs
@@ -1,4 +1,5 @@
-﻿using Castle.DynamicProxy;
+﻿using System;
+using Castle.DynamicProxy;
 
 namespace Harden
 {
@@ -9,6 +10,23 @@ namespace Harden
         public static T Harden<T>(T pansy) where T : class
         {
             return proxyGenerator.CreateClassProxyWithTarget<T>(pansy, new HardenInterceptor());
+        }
+
+        public static T Create<T>() where T : class
+        {
+            return proxyGenerator.CreateClassProxy<T>(new HardenInterceptor());
+        }
+        public static object Create(Type t)
+        {
+            return proxyGenerator.CreateClassProxy(t, new HardenInterceptor());
+        }
+    }
+
+    public static class HardenerExtensions
+    {
+        public static T Harden<T>(this T t) where T : class
+        {
+            return Hardener.Harden(t);
         }
     }
 }
